fe7a6f7763feeb5d07d7a909f793b3d1f3e2781b
jiseokmin <37jr@naver.com>
2025-05-20 23:14:22 +0900

    feat : 홈 탭 테이블 뷰 추가

---
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix Clone
+//
+//  Created by SM J on 5/20/25.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
