3d92d2ab9b3e3cd4dbfb0d84696eee8794ffdb64
jiseokmin <37jr@naver.com>
2025-05-26 23:53:09 +0900

    add : hero header view

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -23,7 +23,14 @@ class HomeViewController: UIViewController {
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
         
-        homeFeedTable.tableHeaderView = UIView(frame: CGRect(x:0, y:0, width: view.bounds.width, height: 450))
+        configureNavbar()
+        
+        let headerView = HeroHeaderUIView(frame: CGRect(x:0, y:0, width: view.bounds.width, height: 450))
+        homeFeedTable.tableHeaderView = headerView
+    }
+    
+    private func configureNavbar() {
+        
     }
     
     override func viewDidLayoutSubviews() {
