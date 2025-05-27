491ae58fdcccb85cd3328c45f6c20ba39d30113b
jiseokmin <37jr@naver.com>
2025-05-27 23:38:06 +0900

    add : sending url requests and parsing json response

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+struct Constants {
+    static let API_KEY = "2bdd4325c1d435efd275abb08f5b9183"
+    static let baseURL = "https://api.themoviedb.org"
+}
+
+enum APIError: Error {
+    case failedTogetData
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/movie/day?api_key=\(Constants.API_KEY)") else {return}
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+}
