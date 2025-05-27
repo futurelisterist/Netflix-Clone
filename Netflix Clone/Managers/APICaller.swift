8b39087675e0719593c9c1334bc7bff8cfa936a9
jiseokmin <37jr@naver.com>
2025-05-27 23:36:55 +0900

    add : sending URL Requests and parsing json respons

---
+++ Netflix Clone/Managers/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  Netflix Clone
+//
+//  Created by SM J on 5/27/25.
+//
+
+import Foundation
---
+++ Netflix Clone/Models/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  Netflix Clone
+//
+//  Created by SM J on 5/27/25.
+//
+
+import Foundation
