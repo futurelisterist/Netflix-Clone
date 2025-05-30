5b547af43cf717ec0c192ab739c7afe64605a0ae
jiseokmin <37jr@naver.com>
2025-05-30 19:54:52 +0900

    feat : consuming api to fetch data for each section

---
+++ Netflix Clone/Models/Tv.swift
@@ -0,0 +1,8 @@
+//
+//  Tv.swift
+//  Netflix Clone
+//
+//  Created by SM J on 5/30/25.
+//
+
+import Foundation
---
+++ Netflix Clone/Resources/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  Netflix Clone
+//
+//  Created by SM J on 5/30/25.
+//
+
+import Foundation
